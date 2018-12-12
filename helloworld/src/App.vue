fac372de16d9e92e031b7ac71c471a229d9ca5a0
Passion-Logan <wangdadamailbox@163.com>
2018-12-12 18:41:56 +0800

    增加表格

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,14 +1,139 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <div id="app">
+      <img src="./assets/logo.png">
+      <router-view/>
+    </div>
+
+    <div style="margin-top: 20px">
+      <el-button @click="toggleSelection()">取消选择</el-button>
+    </div>
+    <el-table
+    ref="multipleTable"
+    :data="tableData"
+    border
+    style="width: 100%"
+    @selection-change="handleSelectionChange">
+      <el-table-column
+        type="selection"
+        width="40">
+      </el-table-column>
+      <el-table-column
+        fixed
+        prop="date"
+        label="日期"
+        width="150">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="province"
+        label="省份"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="city"
+        label="市区"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+        width="300">
+      </el-table-column>
+      <el-table-column
+        prop="zip"
+        label="邮编"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="150">
+        <template slot-scope="scope">
+          <el-button 
+          type="text"
+          size="small"
+          @click="handleClick(scope.row)">查看</el-button>
+          <el-button 
+          type="text"
+          size="small"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+          type="text"
+          size="small"
+          @click="deleteClick(scope.$index, tableData)">移除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: 'App',
+
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          province: '上海',
+          city: '普陀区',
+          address: '上海市普陀区金沙江路 1518 弄',
+          zip: 200333
+        }]
+      }
+    },
+    methods: {
+     toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      handleClick(row) {
+        console.log(row);
+      },
+      deleteClick(index, row) {
+        rows.splice(index, 1);
+      },
+      handleEdit(index, row) {
+        console.log(index, row);
+      }
+    },
+    multipleSelection: []
+  }
 </script>
 
 <style>
