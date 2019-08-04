2b615ce6a06995946ce93b1ea92cdf002c3207e7
Passion-Logan <wangdadamailbox@163.com>
2019-08-04 22:15:08 +0800

    添加修改

--- quartz/src/views/quartz/quartz.vue
+++ quartz/src/views/quartz/quartz.vue
@@ -1,13 +1,8 @@
 <template>
   <div>
     <div style="margin-bottom:10px">
-      <el-button
-        @click="addData"
-        size="mini"
-        type="primary"
-        v-hasPermission="'gdzcMgr_add'"
-      >
-        <svg-icon icon-class="svg-btn-add" /> 新增入库
+      <el-button @click="addData" size="mini" type="primary" v-hasPermission="'gdzcMgr_add'">
+        <svg-icon icon-class="svg-btn-add" />新增任务
       </el-button>
     </div>
 
@@ -15,41 +10,17 @@
       <el-table
         :data="tableData"
         border
-        tooltip-effect='light'
+        tooltip-effect="light"
         style="width: 100%; margin-top: 10px"
         v-loading="tableLoading"
       >
-        <el-table-column
-          prop="rwmc"
-          label="任务名称"
-          :show-overflow-tooltip="true"
-        ></el-table-column>
-        <el-table-column
-          prop="KSSJ"
-          label="任务分组"
-          width="120"
-        ></el-table-column>
-        <el-table-column
-          prop="JSSJ"
-          label="Cron表达式"
-          width="120"
-        ></el-table-column>
-        <el-table-column
-          prop="PDBM"
-          label="执行状态"
-          :show-overflow-tooltip="true"
-        ></el-table-column>
-        <el-table-column
-          prop="PDR"
-          label="备注"
-          width="120"
-        ></el-table-column>
+        <el-table-column prop="rwmc" label="任务名称" :show-overflow-tooltip="true"></el-table-column>
+        <el-table-column prop="KSSJ" label="任务分组" width="120"></el-table-column>
+        <el-table-column prop="JSSJ" label="Cron表达式" width="120"></el-table-column>
+        <el-table-column prop="PDBM" label="执行状态" :show-overflow-tooltip="true"></el-table-column>
+        <el-table-column prop="PDR" label="备注" width="120"></el-table-column>
 
-        <el-table-column
-          label="操作"
-          width="120"
-          fixed="right"
-        >
+        <el-table-column label="操作" width="120" fixed="right">
           <template slot-scope="scope">
             <el-button
               type="text"
@@ -57,11 +28,8 @@
               v-hasPermission="'pdgl_checkZC'"
               :disabled="scope.row.ISSJ === '0'"
               @click="checkPDZC(scope.$index, scope.row)"
-            >
-              查看盘点资产
-            </el-button>
+            >查看盘点资产</el-button>
           </template>
-
         </el-table-column>
       </el-table>
       <el-pagination
@@ -73,8 +41,7 @@
         :page-size="pageSize"
         layout="total, sizes, prev, pager, next, jumper"
         :total="totalNum"
-      >
-      </el-pagination>
+      ></el-pagination>
     </div>
   </div>
 </template>
@@ -82,26 +49,26 @@
 <script >
 export default {
   data() {
-    return {}
+    return {};
   },
   methods: {
     // 初始化表格数据
     getTableData(flag, validateFLag) {
       if (!validateFLag) {
-        this.$refs['term'].validate(vaild => {
+        this.$refs["term"].validate(vaild => {
           if (vaild) {
             if (flag) {
-              this.currentPage = 1
+              this.currentPage = 1;
             }
-            this.getTableDataFlag()
+            this.getTableDataFlag();
           }
-        })
+        });
       } else {
-        this.getTableDataFlag()
+        this.getTableDataFlag();
       }
     }
   }
-}
+};
 </script>
 
 <style>
